805d52a25717ae5f36bec90d3696b4be7c6a5167
Daniel77688 <daniel77688@gmail.com>
2025-09-16 19:58:09 +0200

    Pila con array estatico completada

--- ArrayIntegerStack/src/ArrayIntegerStack.java
+++ ArrayIntegerStack/src/ArrayIntegerStack.java
@@ -1,69 +1,108 @@
 public class ArrayIntegerStack implements IntegerStackInterface {
 
-    public static final int CAPACITY=1000;
+    public static final int CAPACITY = 1000;
     int[] stack;
     int currentSize = 0;
 
-
-    public ArrayIntegerStack(){
+    public ArrayIntegerStack() {
         this.stack = new int[CAPACITY];
     }
-    public ArrayIntegerStack(int size){
+
+    public ArrayIntegerStack(int size) {
         this.stack = new int[size];
     }
 
     /**
-     * Devuelve el tamano de la pila. Es decir, el numero de elementos que se encuentran en la pila.
+     * Devuelve el tamano de la pila. Es decir, el numero de elementos que se
+     * encuentran en la pila.
      * [1, 2, 3] -> tiene tres elementos.
+     * 
      * @return tamano de la pila
      */
     @Override
     public int size() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return this.currentSize;
     }
 
     /**
-     * @return true si la pila esta vacia (no contiene elementos), false en otro caso.
+     * @return true si la pila esta vacia (no contiene elementos), false en otro
+     *         caso.
      */
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return this.currentSize == 0;
     }
 
     /**
-     * Devuelve el primer elemento de la pila, pero no lo elimina. Es decir, no altera el contenido de la estructura.
-     * @return el primer elemento de la pila (el que se devolveria al hacer pop()) o null si la pila esta vacia
+     * Devuelve el primer elemento de la pila, pero no lo elimina. Es decir, no
+     * altera el contenido de la estructura.
+     * 
+     * @return el primer elemento de la pila (el que se devolveria al hacer pop()) o
+     *         null si la pila esta vacia
      */
     @Override
     public Integer top() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (!this.isEmpty()) {
+            return this.stack[0];
+        }
+        return null;
     }
 
     /**
-     * Anade un elemento a la pila. Si no hay espacio suficiente, no anade el elemento y pinta un mensaje por pantalla para informar.
+     * Anade un elemento a la pila. Si no hay espacio suficiente, no anade el
+     * elemento y pinta un mensaje por pantalla para informar.
+     * 
      * @param e
      */
     @Override
     public void push(Integer e) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (this.currentSize >= CAPACITY) {
+            System.err.println("No se puede aniadir el elemento. La pila esta llena.");
+        } else {
+            this.stack[this.currentSize] = e;
+            this.currentSize++;
+        }
     }
 
     /**
      * Devuelve el primer elemento de la pila y lo elimina de la misma.
-     * @return el primer elemento de la pila (el que se devolveria al hacer pop()) o null si la pila esta vacia
+     * 
+     * @return el primer elemento de la pila (el que se devolveria al hacer pop()) o
+     *         null si la pila esta vacia
      */
     @Override
     public Integer pop() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (!this.isEmpty()) {
+            int elemento = this.stack[0];
+            for (int i = 0; i < this.currentSize - 1; i++) {
+                this.stack[i] = this.stack[i + 1];
+            }
+            this.currentSize--;
+            return elemento;
+        }
+        return null;
     }
 
     /**
      * Devuelve una cadena representado el contenido de la pila.
      * Si a una pila se le han anadido los elementos 1, 2 y 3, en ese orden,
      * entonces la cadena resultante en este metodo sera la siguiente: "[1, 2, 3]".
+     * 
      * @return una cadena de texto representando el contenido de la pila.
      */
-    public String toString(){
-        throw new UnsupportedOperationException("Not supported yet.");
+    public String toString() {
+        if (this.isEmpty()) {
+            return "[]";
+        } else {
+            StringBuilder sb = new StringBuilder();
+            sb.append("[");
+            for (int i = 0; i < this.currentSize - 1; i++) {
+                sb.append(this.stack[i]);
+                sb.append(", ");
+            }
+            sb.append(this.stack[this.currentSize - 1]);
+            sb.append("]");
+            return sb.toString();
+        }
     }
 }
